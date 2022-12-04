a95efc3357afd5a4f588842d575e0b6a66bf7842
Katlen <katlen.cost@gmail.com>
2022-12-04 20:38:29 -0300

    Update q2e2.py

--- q2e2.py
+++ q2e2.py
@@ -6,13 +6,16 @@ class Queue:
         return self.items == []
 
     def enqueue(self, item, index):
-        self.items.insert(index,item)
+        self.items.insert(index, item)
 
     def dequeue(self):
         return self.items.pop()
 
     def size(self):
         return len(self.items)
+
+    def remove(self, index):
+        self.items.remove(self.items[index])
 
 queue = Queue()
 
@@ -21,35 +24,23 @@ qtd_realizar = int(input())
 
 new_list = []
 
-def enqueue_simplify(index):
-  queue.enqueue(f'{activities[item]} {activities[item+1]}', index)
-
 for item in range(0, len(activities), 2):
-  if not queue.isEmpty():
-    
-    for qitem in range(0, queue.size()):
-      if [s for s in queue.items if activities[item] in s]:
-        break
-      if activities[item+1] == '4':
-        import pdb;pdb.set_trace()
-      if int(activities[item+1]) < int(queue.items[qitem].split()[1]):
-        enqueue_simplify(qitem+1)
-        
-      elif int(activities[item+1]) == int(queue.items[qitem].split()[1]):
-        enqueue_simplify(qitem-1)
-        
-      else:
-        enqueue_simplify(0)
-  else:
-    enqueue_simplify(0)
-print(queue.items)
+    queue.enqueue([activities[item], activities[item+1]], 0)
 
-
+for tam in range(0, queue.size()):
+    for iitem, qitem in enumerate(queue.items):
+        if iitem+2 <= queue.size():
+            if int(qitem[1]) < int(queue.items[iitem+1][1]):
+                queue.remove(iitem)
+                queue.enqueue(qitem, iitem+1)
 
 for item in range(0, qtd_realizar):
-  queue.dequeue()
+    if queue.isEmpty():
+        break
+    queue.dequeue()
 
 print(f'Tamanho da fila: {queue.size()}')
 
-for item in queue.items:
-  print(f'Atividade: {item}')
+for tam in range(0, queue.size()):
+  item = queue.dequeue()
+  print(f'Atividade: {item[0]}, Prioridade: #{item[1]}')
