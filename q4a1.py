0aa05518646ceec410336520e479fe3608f21b07
Katlen <katlen.cost@gmail.com>
2022-12-18 18:49:46 -0300

    Update q4a1.py

--- q4a1.py
+++ q4a1.py
@@ -3,7 +3,8 @@ entrada = int(input())
 dict = {}
 
 def fibonacci(n):
-  dict.update({n: dict.get(n)+1})
+  valor_atual = dict.get(n)
+  dict.update({n: valor_atual+1 if valor_atual else 1})
   if n <= 1:
     return n
   return fibonacci(n-1) + fibonacci(n-2)
@@ -13,4 +14,3 @@ print(f'fibonacci({entrada}) = {f}.')
 
 for item, valor in dict.items():
   print(f'{valor} chamada(s) a fibonacci({item}).')
-print(lista)
