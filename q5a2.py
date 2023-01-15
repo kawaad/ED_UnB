57e2c1d53f78a2aa3e5ec919f92f0419c07017ec
Katlen <katlen.cost@gmail.com>
2023-01-15 18:42:08 -0300

    Update q5a2.py

--- q5a2.py
+++ q5a2.py
@@ -1,34 +1,12 @@
 n = int(input())
 
-you_died = "You died!"
-in_the_box = "It's in the box!"
-bora_rala = "Bora ralar: {}"
+altura_ideal = 180
+peso_ideal = 75
 
-cases = {}
+dict = {}
 
-for testes in range(n):
-  conteudo = input()
-  ralar = False
-  fora = False
-  for turno in range(3):
-    cont_passado = list(input())
-    for letra in cont_passado:
-      if letra not in conteudo:
-          fora = True
-      else:
-        list_conteudo = list(conteudo)
-        del(list_conteudo[conteudo.find(letra)])
-        conteudo = ''.join(list_conteudo)
-  list_conteudo = list(conteudo)
+for pretendentes in range(n):
+  nome, sobrenome, altura, peso = input().split()
+  dict.update({nome: [sobrenome, int(altura), int(peso)]})
 
-  conteudo = ''.join(set(conteudo))
-    
-  if fora:
-    cases.update({testes: you_died})
-  elif conteudo:
-    cases.update({testes: bora_rala.format(''.join(sorted(conteudo)).upper())})
-  else:
-    cases.update({testes: in_the_box})
-
-for i in cases:
-  print(cases[i])
+print(dict)
