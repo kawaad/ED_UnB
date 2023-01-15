76ec7ff70745f352070c13407a13cfd2f369d0da
Katlen <katlen.cost@gmail.com>
2023-01-15 17:11:51 -0300

    Create q5a2.py

---
+++ q5a2.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+altura_ideal = 180
+peso_ideal = 75
+
+dict = {}
+
+for pretendentes in range(n):
+  nome, sobrenome, altura, peso = input().split()
+  dict.update({nome: [sobrenome, int(altura), int(peso)]})
+
+print(dict)
+  
