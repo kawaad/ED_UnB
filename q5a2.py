4b5bd17ffdf6c082a8b5e362b0ff64e2779bac13
Katlen <katlen.cost@gmail.com>
2023-01-15 22:02:23 -0300

    Update q5a2.py

--- q5a2.py
+++ q5a2.py
@@ -1,10 +1,16 @@
+def altura_ordem(alturas):
+   for fillslot in range(len(alturas)-1,0,-1):
+       positionOfMax=0
+       for location in range(1,fillslot+1):
+           if alturas[location]>alturas[positionOfMax]:
+               positionOfMax = location
+
+       temp = alturas[fillslot]
+       alturas[fillslot] = alturas[positionOfMax]
+       alturas[positionOfMax] = temp
+
 n = int(input())
-
-altura_ideal = 180
-peso_ideal = 75
-
 dict = {}
-
 alturas = []
 pesos = []
 for pretendentes in range(n):
@@ -14,28 +20,14 @@ for pretendentes in range(n):
   distancia_altura = altura - 180 if altura >= 180 else 180 - altura
   distancia_peso = peso - 75 if peso >= 75 else 75 - peso
   alturas.append(distancia_altura)
-  pesos.append(distancia_pesos)
-  dict.update({f'{nome} {sobrenome}': [sobrenome, distancia_altura, distancia_peso]})
+  pesos.append(distancia_peso)
+  dict.update({f'{sobrenome}, {nome}': [sobrenome, distancia_altura, distancia_peso]})
 
-melhor_altura = 0
-
-alturas_ideais = []
-for item in dict:
-  dicio = dict[item]
-  if dicio[1] == min(alturas):
-    alturas_ideais.append(item)
-
-pesos_ideais = []
-for alturas in alturas_ideais:
-  dicio = dict[alturas]
-  if dicio[2] == min(pesos):
-    pesos_ideais.append(item)
-
-for pessoa in pesos_ideais:
-  
-melhor_altura = min(alturas)
+altura_ordem(alturas)
 
 
-
-print(dict)
-
+for altura in alturas:
+  for item in dict:
+    dicio = dict[item]
+    if dicio[1] == altura:
+      print(item)
