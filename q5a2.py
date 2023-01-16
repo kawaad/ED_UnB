4aff79f0336d942dca8e9ebc5e6912f123bafee4
Katlen <katlen.cost@gmail.com>
2023-01-15 21:17:31 -0300

    Update q5a2.py

--- q5a2.py
+++ q5a2.py
@@ -5,8 +5,37 @@ peso_ideal = 75
 
 dict = {}
 
+alturas = []
+pesos = []
 for pretendentes in range(n):
   nome, sobrenome, altura, peso = input().split()
-  dict.update({nome: [sobrenome, int(altura), int(peso)]})
+  altura = int(altura)
+  peso = int(peso)
+  distancia_altura = altura - 180 if altura >= 180 else 180 - altura
+  distancia_peso = peso - 75 if peso >= 75 else 75 - peso
+  alturas.append(distancia_altura)
+  pesos.append(distancia_pesos)
+  dict.update({f'{nome} {sobrenome}': [sobrenome, distancia_altura, distancia_peso]})
+
+melhor_altura = 0
+
+alturas_ideais = []
+for item in dict:
+  dicio = dict[item]
+  if dicio[1] == min(alturas):
+    alturas_ideais.append(item)
+
+pesos_ideais = []
+for alturas in alturas_ideais:
+  dicio = dict[alturas]
+  if dicio[2] == min(pesos):
+    pesos_ideais.append(item)
+
+for pessoa in pesos_ideais:
+  
+melhor_altura = min(alturas)
+
+
 
 print(dict)
+
