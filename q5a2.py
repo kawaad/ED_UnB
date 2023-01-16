3a41bfbad8db8d767eab14cafdd57c2ef1707ec2
Katlen <katlen.cost@gmail.com>
2023-01-15 23:24:51 -0300

    Update q5a2.py

--- q5a2.py
+++ q5a2.py
@@ -6,13 +6,12 @@ lista = []
 for pretendentes in range(n):
   nome, sobrenome, altura, peso = input().split()
   altura = int(altura)
-  peso = int(peso)
-  distancia_altura = altura - 180 if altura >= 180 else 180 - altura
-  distancia_peso = peso - 75 if peso >= 75 else 75 - peso
-  
-  lista.append([nome, sobrenome, distancia_altura, distancia_peso])
+  peso = int(peso)  
+  lista.append([nome, sobrenome, altura, peso])
 
-lista = sorted(lista, key = operator.itemgetter(2, 3))
+'''lista = sorted(lista, key = operator.itemgetter(2, 3, 1))'''
+
+lista.sort(key=lambda x: ( abs(x[2]-180), 75-x[3] if 75-x[3] >= 0 else x[3], x[1], x[0]))
 
 for item in lista:
   print(f'{item[1]}, {item[0]}')
